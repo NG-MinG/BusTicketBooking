ba966c5aa1aceac71e89da6a821b5a2b84cc01da
khangduy017 <khangduy017@gmail.com>
2023-03-23 19:41:35 +0700

    user profile

---
+++ frontend/customer/src/components/UserProfile/UserProfile.js
@@ -0,0 +1,48 @@
+import React from 'react'
+import { BrowserRouter, NavLink, Outlet, Route, Routes } from 'react-router-dom'
+import styles from './UserProfile.module.css'
+import { faUser, faPenToSquare, faKey, faTicket, faClockRotateLeft, faArrowLeft } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import Information from './Information/Information'
+import EditInformation from './EditInformation/EditInformation'
+import ChangePassword from './ChangePassword/ChangePassword'
+import MyTicket from './MyTicket/MyTicket'
+
+export default function UserProfile() {
+
+  const chosen = ({ isActive }) => isActive ? `${styles.chosen} ${styles.item}` : styles.item
+
+  return (
+    <div className={styles['UserProfile']}>
+      <div className={styles['menu']}>
+        <NavLink className={chosen} to='/user-profile'>
+          <FontAwesomeIcon className={styles.icon} icon={faUser} style={{ color: '#417DD8', fontSize: '2.8rem' }} />
+          <p>Thông tin của bạn</p>
+        </NavLink>
+        <NavLink className={chosen} to='/user-profile/edit-information'>
+          <FontAwesomeIcon className={styles.icon} icon={faPenToSquare} style={{ color: '#417DD8', fontSize: '2.8rem' }} />
+          <p>Chỉnh sửa thông tin</p>
+        </NavLink>
+        <NavLink className={chosen} to='/user-profile/change-password'>
+          <FontAwesomeIcon className={styles.icon} icon={faKey} style={{ color: '#417DD8', fontSize: '2.8rem' }} />
+          <p>Thay đổi mật khẩu</p>
+        </NavLink>
+        <NavLink className={chosen} to='/user-profile/my-ticket'>
+          <FontAwesomeIcon className={styles.icon} icon={faTicket} style={{ color: '#417DD8', fontSize: '2.8rem' }} />
+          <p>Vé xe của tôi</p>
+        </NavLink>
+        <NavLink className={chosen} to='/user-profile/ticket-history'>
+          <FontAwesomeIcon className={styles.icon} icon={faClockRotateLeft} style={{ color: '#417DD8', fontSize: '2.9rem' }} />
+          <p>Lịch sử mua vé</p>
+        </NavLink>
+        <NavLink className={styles.item}>
+          <FontAwesomeIcon icon={faArrowLeft} style={{ color: '#417DD8', fontSize: '3.2rem' }} />
+          <p>Đăng xuất</p>
+        </NavLink>
+      </div>
+      <div className={styles['content']}>
+        <Outlet />
+      </div>
+    </div>
+  )
+}
--- frontend/customer/src/routes/AppRoutes.js
+++ frontend/customer/src/routes/AppRoutes.js
@@ -1,12 +1,25 @@
-import {Routes, Route, Navigate} from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import TicketBookingPage from "../pages/TicketBookingPage/TicketBookingPage";
+import UserProfilePage from "../pages/UserProfilePage/UserProfilePage"
+import Information from '../components/UserProfile/Information/Information'
+import EditInformation from '../components/UserProfile/EditInformation/EditInformation'
+import ChangePassword from '../components/UserProfile/ChangePassword/ChangePassword'
+import MyTicket from '../components/UserProfile/MyTicket/MyTicket'
+import TicketHistory from "../components/UserProfile/TicketHistory/TicketHistory";
 
 const AppRoutes = () => {
     return (
-    <Routes>
-        <Route path = "/ticket-booking" element = {<TicketBookingPage/>}>
-        </Route>
-    </Routes>)
+        <Routes>
+            <Route path="/ticket-booking" element={<TicketBookingPage />}>
+            </Route>
+            <Route path="/user-profile" element={<UserProfilePage />}>
+                <Route index element={<Information />} />
+                <Route path='edit-information' element={<EditInformation />} />
+                <Route path='change-password' element={<ChangePassword />} />
+                <Route path='my-ticket' element={<MyTicket />} />
+                <Route path='ticket-history' element={<TicketHistory />} />
+            </Route>
+        </Routes>)
 }
 
 export default AppRoutes;   
