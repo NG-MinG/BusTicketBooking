a3ada66814b7d82e2a8daabbdd96562c5115d4e4
GiaHuy <74665412+giahuy200202@users.noreply.github.com>
2023-03-24 22:50:49 +0700

    update page location

--- frontend/admin/src/components/AccountUser/AccountUser.js
+++ frontend/admin/src/components/AccountUser/AccountUser.js
@@ -1,59 +1,6 @@
-import styles from "./AccountUser.module.css";
-import { NavLink } from "react-router-dom";
-import DashBoardIcon from "../../assets/svg/sidebar/dashboard.svg";
-import ActiveIcon from "../../assets/svg/sidebar/active.svg";
-import AccountIcon from "../../assets/svg/sidebar/account.svg";
-import BusTypeIcon from "../../assets/svg/sidebar/bustype.svg";
-import BusStationIcon from "../../assets/svg/sidebar/busstation.svg";
-import BusRouteIcon from "../../assets/svg/sidebar/busroute.svg";
-import Ticket from "../../assets/svg/sidebar/ticket.svg";
 
 const AccountUser = () => {
-  const sidebarData = [
-    {
-      icon: DashBoardIcon,
-      title: "Dashboard"
-    },
-    {
-      icon: AccountIcon,
-      title: "Tài khoản"
-    },
-    {
-      icon: BusTypeIcon,
-      title: "Loại xe"
-    },
-    {
-      icon: BusStationIcon,
-      title: "Trạm xe"
-    },
-    {
-      icon: BusRouteIcon,
-      title: "Tuyến xe"
-    },
-    {
-      icon: Ticket,
-      title: "Vé xe"
-    },
-  ]
-
-  return (
-    <div className={styles["container"]}>
-      <div className={styles["sidebar-container"]}>
-        <div className={styles["menu-container"]}>
-          {sidebarData.map((data) => (
-            <NavLink to="/admin/account" className={styles["menu-content"]}>
-              <div className={styles["sub-menu-content"]}>
-                <img src={data.icon} className={styles["meun-icon"]}></img>
-                <div className={styles["menu-text"]}>{data.title}</div>
-              </div>
-              <img src={ActiveIcon} style={{ "margin-left": "1.5rem" }} className={styles["meun-icon"]}></img>
-            </NavLink>
-          ))}
-        </div>
-      </div>
-      <div className={styles["content-container"]}></div>
-    </div>
-  );
+  return 1;
 };
 
 export default AccountUser;
