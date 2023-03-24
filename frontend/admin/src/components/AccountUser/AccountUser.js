4d9a1f5a6eca7f5f09d55096e768bf1d43305e73
GiaHuy <74665412+giahuy200202@users.noreply.github.com>
2023-03-24 22:15:51 +0700

    code 24/03

--- frontend/admin/src/components/AccountUser/AccountUser.js
+++ frontend/admin/src/components/AccountUser/AccountUser.js
@@ -1,19 +1,54 @@
 import styles from "./AccountUser.module.css";
 import { NavLink } from "react-router-dom";
-import { ReactComponent as DashBoardIcon } from "../../assets/svg/Dashboard/dashboard.svg";
-import { ReactComponent as ActiveIcon } from "../../assets/svg/Dashboard/active.svg";
+import DashBoardIcon from "../../assets/svg/sidebar/dashboard.svg";
+import ActiveIcon from "../../assets/svg/sidebar/active.svg";
+import AccountIcon from "../../assets/svg/sidebar/account.svg";
+import BusTypeIcon from "../../assets/svg/sidebar/bustype.svg";
+import BusStationIcon from "../../assets/svg/sidebar/busstation.svg";
+import BusRouteIcon from "../../assets/svg/sidebar/busroute.svg";
+import Ticket from "../../assets/svg/sidebar/ticket.svg";
 
 const AccountUser = () => {
+  const sidebarData = [
+    {
+      icon: DashBoardIcon,
+      title: "Dashboard"
+    },
+    {
+      icon: AccountIcon,
+      title: "Tài khoản"
+    },
+    {
+      icon: BusTypeIcon,
+      title: "Loại xe"
+    },
+    {
+      icon: BusStationIcon,
+      title: "Trạm xe"
+    },
+    {
+      icon: BusRouteIcon,
+      title: "Tuyến xe"
+    },
+    {
+      icon: Ticket,
+      title: "Vé xe"
+    },
+  ]
+
   return (
     <div className={styles["container"]}>
       <div className={styles["sidebar-container"]}>
         <div className={styles["menu-container"]}>
-          <NavLink to="/admin/account" className={styles["menu-content"]}>
-            <DashBoardIcon className={styles["meun-icon"]} />
-            <div className={styles["menu-text"]}>Dashboard</div>
-            <ActiveIcon className={styles["meun-icon"]} />
-            <ActiveIcon className={styles["meun-icon"]} />
-          </NavLink>
+          {sidebarData.map((data) => (
+            <NavLink to="/admin/account" className={styles["menu-content"]}>
+              <div className={styles["sub-menu-content"]}>
+                <img src={data.icon} className={styles["meun-icon"]}></img>
+                <div className={styles["menu-text"]}>{data.title}</div>
+              </div>
+              <img src={ActiveIcon} style={{ "margin-left": "1.5rem" }} className={styles["meun-icon"]}></img>
+            </NavLink>
+          ))}
         </div>
       </div>
       <div className={styles["content-container"]}></div>
