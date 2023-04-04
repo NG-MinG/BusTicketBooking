772c080f86b27fc0ca226fe2fbc02cbfb1e6245d
NG-MinG <wgmin.it@outlook.com>
2023-04-05 01:03:18 +0700

    create calendar

--- frontend/admin/src/components/Dashboard/Dashboard.js
+++ frontend/admin/src/components/Dashboard/Dashboard.js
@@ -1,6 +1,15 @@
+import React from 'react';
+
+import classes from './Dashboard.module.css';
+import Calendar from '../Calendar';
 
 const Dashboard = () => {
-    return <h1>Dashboard</h1>;
+    return <div className={classes.main_content}>
+      <h1 className={classes.dashboard_title}>Dashboard</h1>
+      <div style={{display: 'flex'}}>
+      <Calendar />
+      </div>
+    </div>;
   };
   
   export default Dashboard;
