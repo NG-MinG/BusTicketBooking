045e9952ea4913af30f5f48620b3f4649e9e789e
GiaHuy <74665412+giahuy200202@users.noreply.github.com>
2023-04-06 21:10:19 +0700

    update account admin page

--- frontend/admin/src/components/AccountUser/AccountUser.js
+++ frontend/admin/src/components/AccountUser/AccountUser.js
@@ -113,7 +113,25 @@ const AccountUser = () => {
       phone: "0803863927",
       address: "Chung cư 24/16, đường Võ Oanh, P.25, quận Bình Thạnh",
     },
-  ]
+    {
+      id: "a92i13490sa2kkawad",
+      name: "Jonas Scheakmsakdwokas",
+      gender: "Nam",
+      birthday: "30/6/1995",
+      mail: "20127525@student.hcmus.edu.vn",
+      phone: "0803863927",
+      address: "Chung cư 24/16, đường Võ Oanh, P.25, quận Bình Thạnh",
+    },
+    {
+      id: "a92i13490sa2kkawad",
+      name: "Jonas Scheakmsakdwokas",
+      gender: "Nam",
+      birthday: "30/6/1995",
+      mail: "20127525@student.hcmus.edu.vn",
+      phone: "0803863927",
+      address: "Chung cư 24/16, đường Võ Oanh, P.25, quận Bình Thạnh",
+    },
+  ];
   return (
     <div className={styles["a"]}>
       <div className={styles["header-container"]}>
@@ -128,32 +146,46 @@ const AccountUser = () => {
           </a>
         </div>
       </div>
-      <table className={styles["table-containter"]}>
-        <tr>
-          <th>ID</th>
-          <th>HỌ TÊN</th>
-          <th>GIỚI TÍNH</th>
-          <th>NGÀY SINH</th>
-          <th>EMAIL</th>
-          <th>SỐ ĐIỆN THOẠI</th>
-          <th>ĐỊA CHỈ</th>
-          <th></th>
-          <th></th>
-        </tr>
-        {accountData.map((data) => (
-          <tr>
-            <td>{data.id}</td>
-            <td>{data.name}</td>
-            <td>{data.gender}</td>
-            <td>{data.birthday}</td>
-            <td>{data.mail}</td>
-            <td>{data.phone}</td>
-            <td>{data.address}</td>
-            <td><a><img src={DeleteIcon}/></a></td>
-            <td><a><img src={BanIcon}/></a></td>
-          </tr>
-        ))}
-      </table>
+      <div className={styles["listAccount"]}>
+        <table className={styles["table-containter"]}>
+          <thead>
+            <tr>
+              <th>HỌ TÊN</th>
+              <th>GIỚI TÍNH</th>
+              <th>NGÀY SINH</th>
+              <th>EMAIL</th>
+              <th>SỐ ĐIỆN THOẠI</th>
+              <th>ĐỊA CHỈ</th>
+              <th></th>
+              <th></th>
+            </tr>
+          </thead>
+
+          <tbody>
+            {accountData.map((data) => (
+              <tr>
+                <td>{data.name}</td>
+                <td>{data.gender}</td>
+                <td>{data.birthday}</td>
+                <td>{data.mail}</td>
+                <td>{data.phone}</td>
+                <td>{data.address}</td>
+                <td>
+                  <a>
+                    <img src={DeleteIcon} />
+                  </a>
+                </td>
+                <td>
+                  <a>
+                    <img src={BanIcon} />
+                  </a>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+
+      </div>
     </div>
   );
 };
