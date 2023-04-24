0483b9d2593fe389b5558c576e3ffa5b6005fdbd
NG-MinG <wgmin.it@outlook.com>
2023-04-24 16:33:49 +0700

    create register module

--- frontend/customer/src/components/Navbar/index.js
+++ frontend/customer/src/components/Navbar/index.js
@@ -5,15 +5,43 @@ import { NavLink } from 'react-router-dom';
 import Button from '../Button';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPhone } from '@fortawesome/free-solid-svg-icons';
-import LoginForm from '../LoginForm';
+import LoginForm from '../AuthForm';
+import ConfirmOTP from '../AuthForm/ConfirmOTP';
+import SuccessForm from '../AuthForm/SuccessForm';
 
 const Navbar = () => {
     const activeLink = ({ isActive }) => `${classes.link}${(isActive) ? ` ${classes.active}` : ''}`;
 
     const [showLogin, setShowLogin] = useState(false);
+    const [step, setStep] = useState(0);
+    const [success, setSuccess] = useState('');
+    const [userInfo, setUserInfo] = useState({
+        phonenumber: '',
+        password: '',
+        passwordConfirm: ''
+    });
+
+    const closeForm = () => {
+        setShowLogin(false);
+        setStep(0);
+    };
+
+    const nextStep = () => {
+        if (step >= 2) return;
+
+        setShowLogin(true);
+        setStep(prev => prev + 1);
+    };
+
+    // Step 0: Login / Register
+    // Step 2: OTPRegister
+    const authStep = [
+        <LoginForm closeForm={closeForm} nextStep={nextStep} setUserInfo={setUserInfo} setSuccess={setSuccess} />,
+        <ConfirmOTP closeForm={closeForm} userInfo={userInfo} setSuccess={setSuccess} />
+    ];
 
     return <>
-        { showLogin && <LoginForm closeForm={() => setShowLogin(false)} /> }
+        { showLogin && ((['login', 'register'].includes(success)) ? <SuccessForm closeForm={closeForm} setSuccess={setSuccess} type={success} /> : authStep[step]) }
         <nav className={classes.navbar}>
             <NavLink to='/' className={`${classes.title} ${classes.link}`}>Bus Ticket Booking</NavLink>
 
