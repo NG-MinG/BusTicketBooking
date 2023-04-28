4aa96f658f7f6320d40c6a6d93740a55fa5f9544
GiaHuy <74665412+giahuy200202@users.noreply.github.com>
2023-04-28 13:53:11 +0700

    admin account

--- frontend/admin/src/components/AccountUser/AccountUser.js
+++ frontend/admin/src/components/AccountUser/AccountUser.js
@@ -2,136 +2,134 @@ import styles from "./AccountUser.module.css";
 import FilterIcon from "../../assets/svg/Account/filter.svg";
 import DeleteIcon from "../../assets/svg/Account/delete.svg";
 import BanIcon from "../../assets/svg/Account/ban.svg";
+import axios from 'axios'
+import { useState } from "react";
+
 
 const AccountUser = () => {
-  const accountData = [
-    {
-      id: "a92i13490sa2kkawad",
-      name: "Jonas Scheakmsakdwokas",
-      gender: "Nam",
-      birthday: "30/6/1995",
-      mail: "20127525@student.hcmus.edu.vn",
-      phone: "0803863927",
-      address: "Chung cư 24/16, đường Võ Oanh, P.25, quận Bình Thạnh",
-    },
-    {
-      id: "a92i13490sa2kkawad",
-      name: "Jonas Scheakmsakdwokas",
-      gender: "Nam",
-      birthday: "30/6/1995",
-      mail: "20127525@student.hcmus.edu.vn",
-      phone: "0803863927",
-      address: "Chung cư 24/16, đường Võ Oanh, P.25, quận Bình Thạnh",
-    },
-    {
-      id: "a92i13490sa2kkawad",
-      name: "Jonas Scheakmsakdwokas",
-      gender: "Nam",
-      birthday: "30/6/1995",
-      mail: "20127525@student.hcmus.edu.vn",
-      phone: "0803863927",
-      address: "Chung cư 24/16, đường Võ Oanh, P.25, quận Bình Thạnh",
-    },
-    {
-      id: "a92i13490sa2kkawad",
-      name: "Jonas Scheakmsakdwokas",
-      gender: "Nam",
-      birthday: "30/6/1995",
-      mail: "20127525@student.hcmus.edu.vn",
-      phone: "0803863927",
-      address: "Chung cư 24/16, đường Võ Oanh, P.25, quận Bình Thạnh",
-    },
-    {
-      id: "a92i13490sa2kkawad",
-      name: "Jonas Scheakmsakdwokas",
-      gender: "Nam",
-      birthday: "30/6/1995",
-      mail: "20127525@student.hcmus.edu.vn",
-      phone: "0803863927",
-      address: "Chung cư 24/16, đường Võ Oanh, P.25, quận Bình Thạnh",
-    },
-    {
-      id: "a92i13490sa2kkawad",
-      name: "Jonas Scheakmsakdwokas",
-      gender: "Nam",
-      birthday: "30/6/1995",
-      mail: "20127525@student.hcmus.edu.vn",
-      phone: "0803863927",
-      address: "Chung cư 24/16, đường Võ Oanh, P.25, quận Bình Thạnh",
-    },
-    {
-      id: "a92i13490sa2kkawad",
-      name: "Jonas Scheakmsakdwokas",
-      gender: "Nam",
-      birthday: "30/6/1995",
-      mail: "20127525@student.hcmus.edu.vn",
-      phone: "0803863927",
-      address: "Chung cư 24/16, đường Võ Oanh, P.25, quận Bình Thạnh",
-    },
-    {
-      id: "a92i13490sa2kkawad",
-      name: "Jonas Scheakmsakdwokas",
-      gender: "Nam",
-      birthday: "30/6/1995",
-      mail: "20127525@student.hcmus.edu.vn",
-      phone: "0803863927",
-      address: "Chung cư 24/16, đường Võ Oanh, P.25, quận Bình Thạnh",
-    },
-    {
-      id: "a92i13490sa2kkawad",
-      name: "Jonas Scheakmsakdwokas",
-      gender: "Nam",
-      birthday: "30/6/1995",
-      mail: "20127525@student.hcmus.edu.vn",
-      phone: "0803863927",
-      address: "Chung cư 24/16, đường Võ Oanh, P.25, quận Bình Thạnh",
-    },
-    {
-      id: "a92i13490sa2kkawad",
-      name: "Jonas Scheakmsakdwokas",
-      gender: "Nam",
-      birthday: "30/6/1995",
-      mail: "20127525@student.hcmus.edu.vn",
-      phone: "0803863927",
-      address: "Chung cư 24/16, đường Võ Oanh, P.25, quận Bình Thạnh",
-    },
-    {
-      id: "a92i13490sa2kkawad",
-      name: "Jonas Scheakmsakdwokas",
-      gender: "Nam",
-      birthday: "30/6/1995",
-      mail: "20127525@student.hcmus.edu.vn",
-      phone: "0803863927",
-      address: "Chung cư 24/16, đường Võ Oanh, P.25, quận Bình Thạnh",
-    },
-    {
-      id: "a92i13490sa2kkawad",
-      name: "Jonas Scheakmsakdwokas",
-      gender: "Nam",
-      birthday: "30/6/1995",
-      mail: "20127525@student.hcmus.edu.vn",
-      phone: "0803863927",
-      address: "Chung cư 24/16, đường Võ Oanh, P.25, quận Bình Thạnh",
-    },
-    {
-      id: "a92i13490sa2kkawad",
-      name: "Jonas Scheakmsakdwokas",
-      gender: "Nam",
-      birthday: "30/6/1995",
-      mail: "20127525@student.hcmus.edu.vn",
-      phone: "0803863927",
-      address: "Chung cư 24/16, đường Võ Oanh, P.25, quận Bình Thạnh",
-    },
-    {
-      id: "a92i13490sa2kkawad",
-      name: "Jonas Scheakmsakdwokas",
-      gender: "Nam",
-      birthday: "30/6/1995",
-      mail: "20127525@student.hcmus.edu.vn",
-      phone: "0803863927",
-      address: "Chung cư 24/16, đường Võ Oanh, P.25, quận Bình Thạnh",
-    },
-  ];
+
+  const [accountData, setAccountData] = useState([])
+
+  axios.get('127.0.0.1:5000/admin/getaccount').then((res) => {
+    // setData(res.data.data.user)
+    setAccountData(res.data.data.account)
+    // setData(res.data.data.schedules)
+  }).catch(error => {
+    console.log(error)
+  })
+
+  console.log(accountData)
+  // const accountData = [
+  //   {
+  //     id: "a92i13490sa2kkawad",
+  //     name: "Jonas Scheakmsakdwokas",
+  //     gender: "Nam",
+  //     birthday: "30/6/1995",
+  //     mail: "20127525@student.hcmus.edu.vn",
+  //     phone: "0803863927",
+  //     address: "Chung cư 24/16, đường Võ Oanh, P.25, quận Bình Thạnh",
+  //   },
+  //   {
+  //     id: "a92i13490sa2kkawad",
+  //     name: "Jonas Scheakmsakdwokas",
+  //     gender: "Nam",
+  //     birthday: "30/6/1995",
+  //     mail: "20127525@student.hcmus.edu.vn",
+  //     phone: "0803863927",
+  //     address: "Chung cư 24/16, đường Võ Oanh, P.25, quận Bình Thạnh",
+  //   },
+  //   {
+  //     id: "a92i13490sa2kkawad",
+  //     name: "Jonas Scheakmsakdwokas",
+  //     gender: "Nam",
+  //     birthday: "30/6/1995",
+  //     mail: "20127525@student.hcmus.edu.vn",
+  //     phone: "0803863927",
+  //     address: "Chung cư 24/16, đường Võ Oanh, P.25, quận Bình Thạnh",
+  //   },
+  //   {
+  //     id: "a92i13490sa2kkawad",
+  //     name: "Jonas Scheakmsakdwokas",
+  //     gender: "Nam",
+  //     birthday: "30/6/1995",
+  //     mail: "20127525@student.hcmus.edu.vn",
+  //     phone: "0803863927",
+  //     address: "Chung cư 24/16, đường Võ Oanh, P.25, quận Bình Thạnh",
+  //   },
+  //   {
+  //     id: "a92i13490sa2kkawad",
+  //     name: "Jonas Scheakmsakdwokas",
+  //     gender: "Nam",
+  //     birthday: "30/6/1995",
+  //     mail: "20127525@student.hcmus.edu.vn",
+  //     phone: "0803863927",
+  //     address: "Chung cư 24/16, đường Võ Oanh, P.25, quận Bình Thạnh",
+  //   },
+  //   {
+  //     id: "a92i13490sa2kkawad",
+  //     name: "Jonas Scheakmsakdwokas",
+  //     gender: "Nam",
+  //     birthday: "30/6/1995",
+  //     mail: "20127525@student.hcmus.edu.vn",
+  //     phone: "0803863927",
+  //     address: "Chung cư 24/16, đường Võ Oanh, P.25, quận Bình Thạnh",
+  //   },
+  //   {
+  //     id: "a92i13490sa2kkawad",
+  //     name: "Jonas Scheakmsakdwokas",
+  //     gender: "Nam",
+  //     birthday: "30/6/1995",
+  //     mail: "20127525@student.hcmus.edu.vn",
+  //     phone: "0803863927",
+  //     address: "Chung cư 24/16, đường Võ Oanh, P.25, quận Bình Thạnh",
+  //   },
+  //   {
+  //     id: "a92i13490sa2kkawad",
+  //     name: "Jonas Scheakmsakdwokas",
+  //     gender: "Nam",
+  //     birthday: "30/6/1995",
+  //     mail: "20127525@student.hcmus.edu.vn",
+  //     phone: "0803863927",
+  //     address: "Chung cư 24/16, đường Võ Oanh, P.25, quận Bình Thạnh",
+  //   },
+  //   {
+  //     id: "a92i13490sa2kkawad",
+  //     name: "Jonas Scheakmsakdwokas",
+  //     gender: "Nam",
+  //     birthday: "30/6/1995",
+  //     mail: "20127525@student.hcmus.edu.vn",
+  //     phone: "0803863927",
+  //     address: "Chung cư 24/16, đường Võ Oanh, P.25, quận Bình Thạnh",
+  //   },
+  //   {
+  //     id: "a92i13490sa2kkawad",
+  //     name: "Jonas Scheakmsakdwokas",
+  //     gender: "Nam",
+  //     birthday: "30/6/1995",
+  //     mail: "20127525@student.hcmus.edu.vn",
+  //     phone: "0803863927",
+  //     address: "Chung cư 24/16, đường Võ Oanh, P.25, quận Bình Thạnh",
+  //   },
+  //   {
+  //     id: "a92i13490sa2kkawad",
+  //     name: "Jonas Scheakmsakdwokas",
+  //     gender: "Nam",
+  //     birthday: "30/6/1995",
+  //     mail: "20127525@student.hcmus.edu.vn",
+  //     phone: "0803863927",
+  //     address: "Chung cư 24/16, đường Võ Oanh, P.25, quận Bình Thạnh",
+  //   },
+  //   {
+  //     id: "a92i13490sa2kkawad",
+  //     name: "Jonas Scheakmsakdwokas",
+  //     gender: "Nam",
+  //     birthday: "30/6/1995",
+  //     mail: "20127525@student.hcmus.edu.vn",
+  //     phone: "0803863927",
+  //     address: "Chung cư 24/16, đường Võ Oanh, P.25, quận Bình Thạnh",
+  //   },
+  // ];
+
   return (
     <div className={styles["a"]}>
       <div className={styles["header-container"]}>
@@ -164,10 +162,10 @@ const AccountUser = () => {
           <tbody>
             {accountData.map((data) => (
               <tr>
-                <td>{data.name}</td>
+                <td>{data.fullname}</td>
                 <td>{data.gender}</td>
-                <td>{data.birthday}</td>
-                <td>{data.mail}</td>
+                <td>{data.dob}</td>
+                <td>{data.email}</td>
                 <td>{data.phone}</td>
                 <td>{data.address}</td>
                 <td>
