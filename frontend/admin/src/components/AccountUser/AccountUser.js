e6d1ec35103bf2e4742f8728efaf7598cd317e02
GiaHuy <74665412+giahuy200202@users.noreply.github.com>
2023-04-28 16:29:19 +0700

    account with be

--- frontend/admin/src/components/AccountUser/AccountUser.js
+++ frontend/admin/src/components/AccountUser/AccountUser.js
@@ -3,132 +3,40 @@ import FilterIcon from "../../assets/svg/Account/filter.svg";
 import DeleteIcon from "../../assets/svg/Account/delete.svg";
 import BanIcon from "../../assets/svg/Account/ban.svg";
 import axios from 'axios'
-import { useState } from "react";
+import React, { useEffect, useState } from 'react'
 
 
 const AccountUser = () => {
 
   const [accountData, setAccountData] = useState([])
 
-  axios.get('127.0.0.1:5000/admin/getaccount').then((res) => {
-    // setData(res.data.data.user)
-    setAccountData(res.data.data.account)
-    // setData(res.data.data.schedules)
-  }).catch(error => {
-    console.log(error)
-  })
+  useEffect(() => {
+    axios.get(process.env.REACT_APP_ipAddress + '/admin/getaccount')
+    .then((res) => {
+      setAccountData(res.data.data.account)
+    }).catch(error => {
+      console.log(error)
+    })
+  }, [])
 
-  console.log(accountData)
-  // const accountData = [
-  //   {
-  //     id: "a92i13490sa2kkawad",
-  //     name: "Jonas Scheakmsakdwokas",
-  //     gender: "Nam",
-  //     birthday: "30/6/1995",
-  //     mail: "20127525@student.hcmus.edu.vn",
-  //     phone: "0803863927",
-  //     address: "Chung cư 24/16, đường Võ Oanh, P.25, quận Bình Thạnh",
-  //   },
-  //   {
-  //     id: "a92i13490sa2kkawad",
-  //     name: "Jonas Scheakmsakdwokas",
-  //     gender: "Nam",
-  //     birthday: "30/6/1995",
-  //     mail: "20127525@student.hcmus.edu.vn",
-  //     phone: "0803863927",
-  //     address: "Chung cư 24/16, đường Võ Oanh, P.25, quận Bình Thạnh",
-  //   },
-  //   {
-  //     id: "a92i13490sa2kkawad",
-  //     name: "Jonas Scheakmsakdwokas",
-  //     gender: "Nam",
-  //     birthday: "30/6/1995",
-  //     mail: "20127525@student.hcmus.edu.vn",
-  //     phone: "0803863927",
-  //     address: "Chung cư 24/16, đường Võ Oanh, P.25, quận Bình Thạnh",
-  //   },
-  //   {
-  //     id: "a92i13490sa2kkawad",
-  //     name: "Jonas Scheakmsakdwokas",
-  //     gender: "Nam",
-  //     birthday: "30/6/1995",
-  //     mail: "20127525@student.hcmus.edu.vn",
-  //     phone: "0803863927",
-  //     address: "Chung cư 24/16, đường Võ Oanh, P.25, quận Bình Thạnh",
-  //   },
-  //   {
-  //     id: "a92i13490sa2kkawad",
-  //     name: "Jonas Scheakmsakdwokas",
-  //     gender: "Nam",
-  //     birthday: "30/6/1995",
-  //     mail: "20127525@student.hcmus.edu.vn",
-  //     phone: "0803863927",
-  //     address: "Chung cư 24/16, đường Võ Oanh, P.25, quận Bình Thạnh",
-  //   },
-  //   {
-  //     id: "a92i13490sa2kkawad",
-  //     name: "Jonas Scheakmsakdwokas",
-  //     gender: "Nam",
-  //     birthday: "30/6/1995",
-  //     mail: "20127525@student.hcmus.edu.vn",
-  //     phone: "0803863927",
-  //     address: "Chung cư 24/16, đường Võ Oanh, P.25, quận Bình Thạnh",
-  //   },
-  //   {
-  //     id: "a92i13490sa2kkawad",
-  //     name: "Jonas Scheakmsakdwokas",
-  //     gender: "Nam",
-  //     birthday: "30/6/1995",
-  //     mail: "20127525@student.hcmus.edu.vn",
-  //     phone: "0803863927",
-  //     address: "Chung cư 24/16, đường Võ Oanh, P.25, quận Bình Thạnh",
-  //   },
-  //   {
-  //     id: "a92i13490sa2kkawad",
-  //     name: "Jonas Scheakmsakdwokas",
-  //     gender: "Nam",
-  //     birthday: "30/6/1995",
-  //     mail: "20127525@student.hcmus.edu.vn",
-  //     phone: "0803863927",
-  //     address: "Chung cư 24/16, đường Võ Oanh, P.25, quận Bình Thạnh",
-  //   },
-  //   {
-  //     id: "a92i13490sa2kkawad",
-  //     name: "Jonas Scheakmsakdwokas",
-  //     gender: "Nam",
-  //     birthday: "30/6/1995",
-  //     mail: "20127525@student.hcmus.edu.vn",
-  //     phone: "0803863927",
-  //     address: "Chung cư 24/16, đường Võ Oanh, P.25, quận Bình Thạnh",
-  //   },
-  //   {
-  //     id: "a92i13490sa2kkawad",
-  //     name: "Jonas Scheakmsakdwokas",
-  //     gender: "Nam",
-  //     birthday: "30/6/1995",
-  //     mail: "20127525@student.hcmus.edu.vn",
-  //     phone: "0803863927",
-  //     address: "Chung cư 24/16, đường Võ Oanh, P.25, quận Bình Thạnh",
-  //   },
-  //   {
-  //     id: "a92i13490sa2kkawad",
-  //     name: "Jonas Scheakmsakdwokas",
-  //     gender: "Nam",
-  //     birthday: "30/6/1995",
-  //     mail: "20127525@student.hcmus.edu.vn",
-  //     phone: "0803863927",
-  //     address: "Chung cư 24/16, đường Võ Oanh, P.25, quận Bình Thạnh",
-  //   },
-  //   {
-  //     id: "a92i13490sa2kkawad",
-  //     name: "Jonas Scheakmsakdwokas",
-  //     gender: "Nam",
-  //     birthday: "30/6/1995",
-  //     mail: "20127525@student.hcmus.edu.vn",
-  //     phone: "0803863927",
-  //     address: "Chung cư 24/16, đường Võ Oanh, P.25, quận Bình Thạnh",
-  //   },
-  // ];
+  const onBan = (id) =>{
+    axios.post(process.env.REACT_APP_ipAddress + `/admin/banaccount/${id}`)
+    .then((res) => {
+      setAccountData(res.data.data.account)
+    }).catch(error => {
+      console.log(error)
+    })
+  }
+
+  const onDelete = (id) =>{
+    axios.post(process.env.REACT_APP_ipAddress + `/admin/deleteaccount/${id}`)
+    .then((res) => {
+      setAccountData(res.data.data.account)
+    }).catch(error => {
+      console.log(error)
+    })
+  }
+
 
   return (
     <div className={styles["a"]}>
@@ -148,12 +56,13 @@ const AccountUser = () => {
         <table className={styles["table-containter"]}>
           <thead>
             <tr>
-              <th>HỌ TÊN</th>
-              <th>GIỚI TÍNH</th>
-              <th>NGÀY SINH</th>
-              <th>EMAIL</th>
-              <th>SỐ ĐIỆN THOẠI</th>
-              <th>ĐỊA CHỈ</th>
+              <th>Họ tên</th>
+              <th>Giới tính</th>
+              <th>Ngày sinh</th>
+              <th>Email</th>
+              <th>Điện thoại</th>
+              <th>Địa chỉ</th>
+              <th>Trạng thái</th>
               <th></th>
               <th></th>
             </tr>
@@ -168,13 +77,14 @@ const AccountUser = () => {
                 <td>{data.email}</td>
                 <td>{data.phone}</td>
                 <td>{data.address}</td>
+                <td>{data.active ? 'Active': 'Inactive'}</td>
                 <td>
-                  <a>
+                  <a onClick = {()=>{onDelete(data._id)}}>
                     <img src={DeleteIcon} />
                   </a>
                 </td>
                 <td>
-                  <a>
+                  <a onClick = {()=>{onBan(data._id)}}>
                     <img src={BanIcon} />
                   </a>
                 </td>
