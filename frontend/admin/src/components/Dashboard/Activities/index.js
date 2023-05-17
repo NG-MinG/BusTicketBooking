67088ec2ec7f363c033b802ef72f89ed137c3e43
NG-MinG <wgmin.it@outlook.com>
2023-05-18 03:21:41 +0700

    fix layout

--- frontend/admin/src/components/Dashboard/Activities/index.js
+++ frontend/admin/src/components/Dashboard/Activities/index.js
@@ -3,6 +3,8 @@ import React, { useEffect, useState } from 'react';
 import classes from './Activities.module.css';
 import axios from 'axios';
 import { toPriceFormat } from '../../../utils/format';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faArrowRight } from '@fortawesome/free-solid-svg-icons';
 
 const Activities = () => {
 
@@ -11,7 +13,9 @@ const Activities = () => {
     useEffect(() => {
         axios.get(`${process.env.REACT_APP_API_HOST}/admin/ticket-history/ticketHistory`)
         .then((res) => {
-            setActivities(res.data.data.ticketHistory);
+            const currentDate = new Date();
+            // setActivities(res.data.data.ticketHistory);
+            setActivities(res.data.data.ticketHistory.filter(el => el.date === `${currentDate.getDate()}/${currentDate.getMonth() + 1}/${currentDate.getFullYear()}`));
         })
         .catch(err => console.log(err));
     }, []);
@@ -64,17 +68,17 @@ const Activities = () => {
     //     }
     // ]
 
-    return <div className={classes.activities}>
+    return <div className={classes.activities} style={{ width: '41%' }}>
         <div className={classes.header}>
             <b>Hoạt động gần đây</b>
         </div>
-        <div className={classes.main_content}>
+        <div className={classes.main_content} style={{ width: '100%' }}>
         {
-            activities_list.map((item, index) => {
+            activities_list && activities_list.length ? activities_list.map((item, index) => {
                 return <div key={`activity_${index}`} className={classes.sub_item}>
                     <div className={classes.details}>
                         <b>{item.guestInfo.name}</b>
-                        <span>{item.departure_city} -&gt; {item.arrival_city}</span>
+                        <span>{item.departure_city} <FontAwesomeIcon icon={faArrowRight} /> {item.arrival_city}</span>
                         <span>{ item.time.split(':').join('h') } - Ghế { item.chosen_seats.join(', ') } - { toPriceFormat(item.total_price) }đ</span>
                         <i>{ item.date.split('/').join(' - ') }</i>
                     </div>
@@ -82,7 +86,14 @@ const Activities = () => {
                         <button className={(item.stage === 'Đang xử lí') ? classes.processing : ((item.stage === 'Đã huỷ') ? classes.canceled : '')} type='button'>{ item.stage }</button>
                     </div>
                 </div>
-            })
+            }) : 
+            <div className={classes.sub_item} >
+                <div className={classes.details}>
+                    Không có dữ liệu
+                </div>
+                <div className={classes.control}>
+                </div>
+            </div>
         }
         </div>
     </div>
