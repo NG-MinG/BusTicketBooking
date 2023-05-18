41c55b0addb7b76b235977a2a09840572327105b
NG-MinG <wgmin.it@outlook.com>
2023-05-18 16:23:37 +0700

    auth login admin / analyst dât

--- frontend/admin/src/routes/AppRoutes.js
+++ frontend/admin/src/routes/AppRoutes.js
@@ -8,25 +8,37 @@ import ManageTicketPage from "../pages/ManageTicketPage/ManageTicketPage";
 import ManageTicketOrderPage from "../pages/ManageTicketOrderPage/ManageTicketOrderPage";
 import ManageTicketOrderEditPage from "../pages/ManageTicketOrderPage/ManageTicketOrderEditPage/ManageTicketOrderEditPage";
 import ManageTicketPageDetail from "../pages/ManageTicketPage/Details/ManageTicketPageDetail";
+import { auth } from '../utils/storage';
+import Login from "../pages/Login/Login";
 
 const AppRoutes = () => {
-    return (
-        <Routes>
-            <Route path="/admin" element={<AdminLayout />}>
-                <Route path="dashboard" element={<DashboardPage />} />
-                <Route path="account" element={<AccountUserPage />} />
-                <Route path="bus-station" element={<BusStationPage />} />
-                <Route path="bus-route" element={<BusRoutePage />} />
-                <Route path="manage-ticket" element={<ManageTicketPage />} />
-                <Route path="manage-ticket/ticket" element={<ManageTicketPage />} />
-                <Route path="manage-ticket/ticket/details/:id" element={<ManageTicketPageDetail />} />
-                <Route path="manage-ticket/ticket-order" element={<ManageTicketOrderPage />} />
-                <Route path="manage-ticket/ticket-order/edit/:id" element={<ManageTicketOrderEditPage />} />
+    const isLogin = auth.isLogin();
+    if (isLogin) {
+        return (
+            <Routes>
+                <Route path="/admin" element={<AdminLayout />}>
+                    <Route path="dashboard" element={<DashboardPage />} />
+                    <Route path="account" element={<AccountUserPage />} />
+                    <Route path="bus-station" element={<BusStationPage />} />
+                    <Route path="bus-route" element={<BusRoutePage />} />
+                    <Route path="manage-ticket" element={<ManageTicketPage />} />
+                    <Route path="manage-ticket/ticket" element={<ManageTicketPage />} />
+                    <Route path="manage-ticket/ticket/details/:id" element={<ManageTicketPageDetail />} />
+                    <Route path="manage-ticket/ticket-order" element={<ManageTicketOrderPage />} />
+                    <Route path="manage-ticket/ticket-order/edit/:id" element={<ManageTicketOrderEditPage />} />
+                    <Route path="*" element={<Navigate to='/admin/dashboard' />} />
+                </Route>
                 <Route path="*" element={<Navigate to='/admin/dashboard' />} />
-            </Route>
-            <Route path="*" element={<Navigate to='/admin/dashboard' />} />
-        </Routes>
-    )
+            </Routes>
+        )
+    } else {
+        return (
+            <Routes>
+                <Route path="/admin/login" element={<Login />} />
+                <Route path="*" element={<Navigate to='/admin/login' />} />
+            </Routes>
+        )
+    }
 }
 
 export default AppRoutes;   
